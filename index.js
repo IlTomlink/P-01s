e3e7f83676632a83fd8f5841721b6d6eabe3abcb
IlTomlink <40507932+IlTomlink@users.noreply.github.com>
2018-06-23 03:25:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disabeEveryone: true});
 
 bot.on("ready", async () => {
-	console.log('${bot.user.username} is online!')		
+	console.log('P-01s è online!')		
 	bot.user.setActivity("cercare le Armi dei Peccati Capitali")
 	bot.user.setStatus('Online')
 });
 
-
-
+client.login(process.env.NDU5ODI0MzQ4OTkyMjQxNjY0.Dg8s8Q.qUDrjqwmOGs-Lb1GnAJlljnTCjE);
 bot.login(botconfig.token);
