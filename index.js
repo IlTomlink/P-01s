872dd94710b3a33f531b44d71a041b3c814184ce
IlTomlink <40507932+IlTomlink@users.noreply.github.com>
2018-06-23 12:33:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 const BOT_TOKEN =_process.env.BOT_TOKEN;
 
 client.on('ready', () => {	
-	console.log("Sono pronta, Maestro Tori.")
+	console.log("Sono pronta, Padron Tori.")
 	bot.user.setActivity("cercare le Armi dei Peccati Capitali")
 	bot.user.setStatus('Online')
 });
